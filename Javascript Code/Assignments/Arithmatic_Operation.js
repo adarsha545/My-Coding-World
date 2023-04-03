6dfbc14e69bf57b74e48248f5e738895aab3a12a
adarsha545 <adarshaghosh545@gmail.com>
2023-04-03 10:38:15 +0530

    commit

---
+++ Javascript Code/Assignments/Arithmatic_Operation.js
@@ -0,0 +1,7 @@
+let a = +prompt("Enter the 1st number :");
+let b = +prompt("Enter the 2nd number :");
+console.log("Sum: ", a + b);
+console.log("Subtraction: ", a - b);
+console.log("Multiplication: ", a * b);
+console.log("Division: ", a / b);
+console.log("Remainder: ", a % b);
---
+++ Javascript Code/Assignments/Swapping.js
@@ -0,0 +1,20 @@
+//let a = prompt("Enter a :");
+//let b = prompt("Enter b :");
+let a= 2;
+let b=3;
+console.log("Before Swapping :")
+console.log("a:", a);
+console.log("b:", b);
+let temp = a;
+a = b;
+b = temp;
+console.log("After Swapping :")
+console.log("a:", a);
+console.log("b:", b);
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("After Swapping :")
+console.log("a:", a);
+console.log("b:", b);
+
