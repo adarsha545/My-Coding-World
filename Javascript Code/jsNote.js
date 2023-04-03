1d96a52504f546cc937fb2f25eacd956c40c6fce
adarsha545 <adarshaghosh545@gmail.com>
2023-04-03 20:02:13 +0530

    comit

--- Javascript Code/jsNote.js
+++ Javascript Code/jsNote.js
@@ -106,11 +106,89 @@ console.log(arr);
 //Both pop & shift return the deleted element
 //(Push & Pop) is faster than (Shift & Unshift),bcz after adding/deleting it shifts the remaining items
 
+//Array Clone
+let array1 = [1, 2, 3, 4];
 
+//process-1
+let array2 = [1, 2, 3, 4];
 
+//process-2
+let array3 = array1.slice(0);
+array3 = array1.slice(0).concat([5, 6]);
 
+//process-3
+let array4 = [].concat(array1);
 
+//process-4
+let array5 = [...array1, ...array2, "nice"];
+console.log(array1, array2, array3, array4, array5);
 
+//In constant array you can add/delete element,but you can't change the elements
 
+//For of loop -> (By default it gives item)
+for (let i of array5) {
+    console.log(i);
+}
 
+//For in loop-> (By default it gives index)
+for (let i in array5) {
+    console.log(i);
 
+}
+//Converting index to item
+for (let i in array5) {
+    console.log(array5[i]);
+
+}
+
+//Array Destructring
+let ar = ["ad", "ar", "sh", "a"];
+let [val1, , val2, , val3] = ar;
+console.log(val1);
+console.log(val2);
+//No iem present to assign, so it is undefined
+console.log(val3);
+
+//Objects are reference type
+//Objects stored key value pairs
+//objects don't have index
+//createing object -> person
+let person = { name: "Adarsha", age: 20, "His hobbies": ["anime", "coding"] };
+console.log(person);
+console.log(person.age);
+console.log(person.hobbies);
+
+//To add new pair
+person.gender = "Male";
+console.log(person);
+
+//Another way to acess data
+//By default key is string in Js
+console.log(person["name"]);
+
+//Advantage of using Bracket, if here we use dot then it give error bcz of space
+console.log(person["His hobbies"]);
+let k = "email";
+
+//If we want to add email as key,not k as key then..inside bracket dont write k in double cot,write simple
+person[k] = "adarshaghosh@gmail.com";
+console.log(person);
+
+//To iterate object->
+//process-1 (without key)
+for (let key in person) {
+    console.log(person[key]);
+}
+
+//process-2 (with key pair)
+for (let key in person) {
+    console.log(`${key} : ${person[key]}`);
+}
+
+//To print only the keys
+console.log(Object.keys(person));
+
+//another way to print 
+for (let key of Object.keys(person)) {
+    console.log(person[key]);
+}
